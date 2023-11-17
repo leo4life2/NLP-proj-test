57ff66c5b7a8d8c9f76d980344d3fcb6b8b02e6e
Leo Li <leo4life@163.com>
2023-11-16 19:25:46 -0500

    update

--- test_train.py
+++ test_train.py
@@ -11,9 +11,15 @@ dataset = load_dataset("yelp_review_full")
 tokenizer = AutoTokenizer.from_pretrained("adept/fuyu-8b")
 
 def process_function(examples):
-    output = tokenizer(examples["text"])
+    output = tokenizer(examples["text"])    
     # Create a mask where the condition is True
     mask = output["input_ids"] == tokenizer.vocab["<s>"]
+    
+    print("type of input ids", type(output["input_ids"]))
+    print("input ids shape", output["input_ids"].shape)
+    print("tokenizer vocab", tokenizer.vocab)
+    print("mask", mask)
+    
     # Find the position of the first occurrence of <s>
     position = mask.nonzero(as_tuple=True)[0][0]
 
